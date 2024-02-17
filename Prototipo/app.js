c4e0740cb492b559314895cc3850acd80f4a7b09
Ryan-Serra <120741341+Ryan-Serra@users.noreply.github.com>
2024-02-17 16:57:03 -0300

    Update app.js

--- Prototipo/app.js
+++ Prototipo/app.js
@@ -3,39 +3,91 @@ var Filtro_ModeloTrabalho = document.querySelectorAll("input[type='checkbox'][na
 // Seletor corrigido para os checkboxes de Localidade
 var Filtro_Localidade = document.querySelectorAll("input[type='checkbox'][name='filtroEstado']");
 // Seletor corrigido para os checkboxes de Empresa
-var Filtro_Empresas = document.querySelectorAll("input[type='checkbox'][name='Nome Empresas']");
-// label para efetuar as comparações com o modelo de trabalho
-var label_ModeloTrabalho = document.querySelectorAll('label[for="modetrabalho"]')
-// label para efetuar as comparações  com a localidade
-var label_Localidade= document.querySelectorAll( 'label[for="Estados"]' )
-// label para efetuar as comparações com as empresas
-var label_Empresas= document.querySelectorAll( "label[for='NomesEmpresas']" );
+var Filtro_Empresas = document.querySelectorAll("input[type='checkbox'][name='Nome_Empresas']");
+// capa dos processos seletivos
 var capa_ps=document.querySelectorAll('.capa_ps');
+// div dos requisitos do processos seletivos
 var requisito_ps= document.querySelectorAll( '.Requisitos' );
+// Todos os paragrafos que terá a localidade
 var Localidade=document.querySelectorAll('.Localidade');
+// Todos os paragrafos que terá o nome da empresa
 var Empresa=document.querySelectorAll('.Empresa');
+// Todos os paragrafos que terá o modelo de trabalho 
 var ModeloTrabalho=document.querySelectorAll('.modelotrabalho');
-var procura= document.querySelector('#busca').value
-console.log(Localidade[0].innerHTML);
+// input de busca 
+var busca=document.querySelector('#busca')
+// auxiliares  para realizar a busca
+var busca_Localidade=''
+var busca_Empresas=''
+var busca_ModeloTrabalho=''
+var procura=''
+for(let i=0; i<Localidade.length; i++){
+busca.addEventListener('keypress', function(event){
+    capa_ps[i].style.display='none'
+    procura= busca.value
+    procura=procura.toUpperCase()
+    busca_Localidade= Localidade[i].innerHTML;
+    busca_Localidade=busca_Localidade.toUpperCase();
+    if(event.key==='Enter'){
+        for(let j=0; j<Localidade.length;j++)
+    if (busca_Localidade.includes(procura)) {
+        capa_ps[i].style.display='block'
+    }
+    
+    
+}});
+}
+for(let i=0; i<ModeloTrabalho.length;i++){
+    busca.addEventListener('keypress', function(event){
+        capa_ps[i].style.display='none'
+        procura=busca.value
+        procura=procura.toUpperCase()
+        busca_ModeloTrabalho= ModeloTrabalho[i].innerHTML;
+        busca_ModeloTrabalho=busca_ModeloTrabalho.toUpperCase();
+        if(event.key=='Enter'){
+            if(busca_ModeloTrabalho.includes(procura)){
+               capa_ps[i].style.display='block';
+        }
+        }
+    })
+}
+for(let i=0; i<Empresa.length;i++){
+    busca.addEventListener('keypress', function(event){
+        procura=busca.value
+        procura=procura.toUpperCase()
+        busca_Empresas= Empresa[i].innerHTML;
+        busca_Empresas=busca_Empresas.toUpperCase();
+        if(event.key=='Enter'){
+            if(busca_Empresas.includes(procura)){
+                capa_ps[i].style.display='block';
+        }
+        }
+    })
+}
 
-busca.addEventListener('enter', function(){
-    if(Localidade.includes(procura))
-        console.log ('achou na localidade');
-});
 for(let i=0; i < capa_ps.length; i++ ){
     capa_ps[i].addEventListener('click',function(){
         requisito_ps[i].style.display='block';
-        console.log("funcionou")
+        
     });
 }
 
+
 // Adicionando listener de evento de mudança para o filtro de Modelo de Trabalho
-  /* for(let i=0;i< Filtro_ModeloTrabalho.length; i++){
-        Filtro_ModeloTrabalho[i].addEventListener('change',function() {
-          if(Filtro_ModeloTrabalho[i].checked)
-          console.log(label_ModeloTrabalho[i].getAttribute('for'))
-    });
-    }*/
+   for(let i=0;i< Filtro_ModeloTrabalho.length; i++){
+        Filtro_ModeloTrabalho[i].addEventListener('click',function(event) {
+            procura= Filtro_ModeloTrabalho[i].id 
+            procura=procura.toUpperCase()
+            console.log(procura)
+            busca_ModeloTrabalho= ModeloTrabalho[i].innerHTML;
+            busca_ModeloTrabalho=busca_ModeloTrabalho.toUpperCase()
+          if(event.key=='change'){
+            if(busca_ModeloTrabalho.includes(procura)) {
+                console.log('funcionou')
+          }
+          
+    }});
+    }
     // Adicionando listener de evento de mudança para o filtro de Localidade
 /*    Filtro_Localidade.forEach(function(item) {
         item.addEventListener('change', function () {
