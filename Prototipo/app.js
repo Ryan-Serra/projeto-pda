80f0aef0c4a489772ad942a6f9decce0d9d8db5a
Ryan-Serra <120741341+Ryan-Serra@users.noreply.github.com>
2024-02-20 09:44:30 -0300

    Update app.js

--- Prototipo/app.js
+++ Prototipo/app.js
@@ -1,11 +1,7 @@
-// Seletor corrigido para os checkboxes do Modelo de Trabalho
-var Filtro_ModeloTrabalho = document.querySelectorAll("input[type='checkbox'][name='modetrabalho']");
+
 //  Eventos de click nos checkboxes
 var checkboxes = document.querySelectorAll('input[type="checkbox"]:checked');
 // Seletor corrigido para os checkboxes de Localidade
-var Filtro_Localidade = document.querySelectorAll("input[type='checkbox'][name='filtroEstado']");
-// Seletor corrigido para os checkboxes de Empresa
-var Filtro_Empresas = document.querySelectorAll("input[type='checkbox'][name='Nome_Empresas']");
 // capa dos processos seletivos
 var capa_ps=document.querySelectorAll('.capa_ps');
 // div dos requisitos do processos seletivos
@@ -18,6 +14,8 @@ var Empresa=document.querySelectorAll('.Empresa');
 var ModeloTrabalho=document.querySelectorAll('.modelotrabalho');
 // input de busca 
 var busca=document.querySelector('#busca')
+// Todos  os filtros juntos 
+ var filtros= document.querySelectorAll(".filtros")
 // auxiliares  para realizar a busca
 var busca_Localidade=''
 var busca_Empresas=''
@@ -25,6 +23,7 @@ var busca_ModeloTrabalho=''
 var procura=''
 var contador=0
 var control=0
+var buscador=''
 var EstadoCheckbox=[false, false, false]
 for(let i=0; i<Localidade.length; i++){
 busca.addEventListener('keypress', function(event){
@@ -79,27 +78,41 @@ for(let i=0; i < capa_ps.length; i++ ){
 }
 
 // Adicionando listener de evento de mudança para o filtro de Modelo de Trabalho
- for (let i = 0; i < Filtro_ModeloTrabalho.length; i++) {
-        Filtro_ModeloTrabalho[i].addEventListener('change', function(event) {
-            procura = Filtro_ModeloTrabalho[i].id.toUpperCase();
+ for (let i = 0; i < filtros.length; i++) {
+        filtros[i].addEventListener('change', function(event) {
+            procura = filtros[i].id.toUpperCase();
     
             // Verificar o estado dos checkboxes e atualizar o EstadoCheckbox
-            EstadoCheckbox[i] = Filtro_ModeloTrabalho[i].checked;
+            EstadoCheckbox[i] = filtros[i].checked;
     
             // Verificar se pelo menos um checkbox está marcado
             let algumCheckboxMarcado = EstadoCheckbox.some(checked => checked);
     
             for (let j = 0; j < ModeloTrabalho.length; j++) {
-                busca_ModeloTrabalho = ModeloTrabalho[j].innerHTML.toUpperCase();
+              //  buscador = ModeloTrabalho[j].innerHTML.toUpperCase();
     
                 // Verificar se o modelao de trabalho corresponde ao filtro
                 let modeloMarcado = false;
                 for (let r = 0; r < EstadoCheckbox.length; r++) {
-                    if (EstadoCheckbox[r] && busca_ModeloTrabalho.includes(Filtro_ModeloTrabalho[r].id.toUpperCase())) {
+                    buscador = ModeloTrabalho[j].innerHTML.toUpperCase();
+                    if (EstadoCheckbox[r] && buscador.includes(filtros[r].id.toUpperCase())) {
                         modeloMarcado = true;
                         break;
                     } 
+                    
+                    buscador=Empresa[j].innerHTML.toUpperCase()
+                    if(EstadoCheckbox[r] && buscador.includes(filtros[r].id.toUpperCase())) {
+                        modeloMarcado=true;
+                        break;
+                    }
+                    buscador=Localidade[j].innerHTML.toUpperCase()
+                    
+                    if(EstadoCheckbox[r] && buscador.includes(filtros[r].id.toUpperCase())) {
+                        modeloMarcado=true;
+                        break;
+                    }
                 }
+            
     
                 // Mostrar ou ocultar a vaga com base no filtro de modelo de trabalho
                 if (algumCheckboxMarcado && modeloMarcado) {
@@ -114,7 +127,7 @@ for(let i=0; i < capa_ps.length; i++ ){
     }
    
     // Adicionando listener de evento de mudança para o filtro de Localidade
-    for (let i = 0; i < Filtro_Localidade.length; i++) {
+/*    for (let i = 0; i < Filtro_Localidade.length; i++) {
         Filtro_Localidade[i].addEventListener('change', function(event) {
             procura = Filtro_Localidade[i].id.toUpperCase();
     
@@ -147,10 +160,10 @@ for(let i=0; i < capa_ps.length; i++ ){
             }
         });
     }
-  
+  */
 
 // Adicionando o evento para filtrar pelas Empresas
-for (let i = 0; i < Filtro_Empresas.length; i++) {
+/*for (let i = 0; i < Filtro_Empresas.length; i++) {
     Filtro_Empresas[i].addEventListener('change', function(event) {
         procura = Filtro_Empresas[i].id.toUpperCase();
 
@@ -183,5 +196,5 @@ for (let i = 0; i < Filtro_Empresas.length; i++) {
         }
     });
 }
-  
+*/  
  // Função para aplicar todos os filtros
