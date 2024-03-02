fe7ed74aa6d59538f0205f55ac9738f283afb055
Erik-M-D <138533934+Erik-M-D@users.noreply.github.com>
2024-03-02 16:10:36 -0300

    Update a_final.py

--- a_final.py
+++ a_final.py
@@ -1,30 +1,19 @@
 from flask import Flask, render_template, request, jsonify, redirect, url_for
 import pandas as pd
 import json
-import os
-from datetime import date
-from werkzeug.utils import secure_filename
-from convert_df import dataframes
 
 
-
-# import database
-
-# IMPORTAÇÃO DA PLANILHA 
-df = pd.read_excel('./Vagas.xlsx')
-
-
-# #PAGINA INICIAL ONDE MOSTRA A PLANILHA PODENDO SER FILTRADA, PERMITINDO ENVIO DE ARQUIVOS
+# PERMITINDO ENVIO DE ARQUIVOS
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     
-    df_base = dataframes()
-    
-    return render_template('index.html', data=df_base.to_json(orient='records'))
- 
+    df = pd.read_excel('./output/Vagas.xlsx')
+
+    #return render_template('index.html', data=df_base.to_json(orient='records'))
+     return data=df.to_json(orient='records')
 
 #caminho para testes, AINDA NÃO FINALIZADO!
 if __name__ == "__main__":
