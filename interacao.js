c6785d9a1c7a1a23c9535b4cf6ca51971891693f
Ryan-Serra <120741341+Ryan-Serra@users.noreply.github.com>
2024-04-13 08:21:37 -0300

    Update interacao.js
    Update do filtro resposivo

--- interacao.js
+++ interacao.js
@@ -8,14 +8,9 @@
 */
 // capa dos processos seletivos
 var capa_ps=document.querySelectorAll('.capa_ps');
-// div dos requisitos do processos seletivos
-// var requisito_ps= document.querySelectorAll( '.Requisitos' );
-// // Botão para voltar a página inicial
-// var voltar=document.querySelectorAll('.btn_voltar');
-//Botão para abrir o input text responsivo
 var busca_responsive = document.querySelector('.busca_reponsive');
 //Interação com os filtros
-var div_filtros=document.querySelector("#filtros")
+
 // Adicionar um evento de clique a busca_responsive
 busca_responsive.addEventListener('click', function() {
     // Lógica para manipular o clique no elemento busca_responsive
@@ -25,33 +20,8 @@ busca_responsive.addEventListener('click', function() {
 });
 if(window.innerWidth<=475){
     busca.style.display='none'
-    div_filtros.style.display='none'
+    
 }
-//Função responsável por abrir a aba de requisitos
-// for(let i=0; i < capa_ps.length; i++ ){
-//     //capa_ps[i].style.display = 'none'
-//     capa_ps[i].addEventListener('click',function(){
-//        if(requisito_ps[i].style.display=='block') requisito_ps[i].style.display='none'
-//        else requisito_ps[i].style.display='block';
-//        for(let j=0; j<capa_ps.length ;j++) {
-//          capa_ps[j].style.display='none'
-//     }
-//     div_filtros.style.display='none'
-// });
-// }
-// // Função responsável por fechar a aba de requisitos 
-// for(let i=0; i <voltar.length;i++){
-//     voltar[i].addEventListener("click", function (e) {
-//         if(requisito_ps[i].style.display=='block' ) {
-//             requisito_ps[i].style.display='none'
-//             for(let j=0; j< capa_ps.length;j++)
-//                 capa_ps[j].style.display='block'
-//            if( window.innerWidth>475) div_filtros.style.display='block'        
-//             }
-       
-//     });
-// }
-
 
 
 var modalBtns = document.querySelectorAll(".modal-open");
